b6c23f6cf8002cee7df82376e38f7ea1a19b50d9
AyaazAhamad <100121514+AyaazAhamad@users.noreply.github.com>
2022-07-02 19:50:52 +0530

    def prime(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1     if c==2:         return 1     else:         return 0 def rev(a):     su=0     while a>0:         rem=a%10         su=(su*10)+rem         a//=10     return su a=int(input()) arev=rev(a) if prime(a)==1:     if prime(arev)==1:         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,21 +1,25 @@
-n=int(input())
-count=0
-rev=0
-for i in range(1,n+1):
-  if(n%i==0):
-     count+=1
-if(count==2):
-  while(n!=0):
-    rem=n%10
-    rev=rev*10+rem
-    n//=10
-  fcount=0
-  for j in range(1,rev+1):
-    if(rev%j==0):
-       fcount+=1
-  if(fcount==2):
-      print("circular prime")
-  else:
-      print("prime but not a circular prime")
+def prime(a):
+    c=0
+    for i in range(1,a+1):
+        if a%i==0:
+            c+=1
+    if c==2:
+        return 1
+    else:
+        return 0
+def rev(a):
+    su=0
+    while a>0:
+        rem=a%10
+        su=(su*10)+rem
+        a//=10
+    return su
+a=int(input())
+arev=rev(a)
+if prime(a)==1:
+    if prime(arev)==1:
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
-      print("not prime")
+    print("not prime")
