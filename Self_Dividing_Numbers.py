2b88d9829adcefa3297088c129b3ef4e6e0e3d92
AyaazAhamad <100121514+AyaazAhamad@users.noreply.github.com>
2022-07-02 20:10:30 +0530

    a=int(input()) b=int(input()) for i in range(a,b+1):     v=i     j=i     s=0     count=0     while(i>0):      s=i%10      count=count+1      i=i//10     c=0     while j>0:      r=j%10      if(r!=0 and v%r==0):         c=c+1      j=j//10     if(count==c):       print(v,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,23 +1,19 @@
-def selfdiv(a):
-    temp=a
-    flag=0
-    while temp>0:
-        rem=temp%10
-        if rem==0:
-            flag=0
-            break
-        if a%rem==0:
-            flag=1
-            temp//=10
-        else:
-            flag=0
-            break
-    if flag==1:
-        return 1
-    else:
-        return 0
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    if selfdiv(i)==1:
-        print(i,end=" ")
+    v=i
+    j=i
+    s=0
+    count=0
+    while(i>0):
+     s=i%10
+     count=count+1
+     i=i//10
+    c=0
+    while j>0:
+     r=j%10
+     if(r!=0 and v%r==0):
+        c=c+1
+     j=j//10
+    if(count==c):
+      print(v,end=" ")
